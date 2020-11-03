c79b926d4f858fa78dee10c2421e7cae122a8b71
Marioal1206 <L20100257@nlaredo.tecnm.mx>
2020-11-02 23:15:54 -0600

    Add Prueva

---
+++ trabajo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
